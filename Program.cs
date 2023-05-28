a11e1d5860fe0045d600585ef325ecd5d3f1e8c8
enes-kesen <52306218+enes-kesen@users.noreply.github.com>
2023-05-28 19:00:05 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using FCC_TasarımO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DroneController droneController = new DroneController();
+        droneController.ControlDrone();
+    }
+
+}
+
