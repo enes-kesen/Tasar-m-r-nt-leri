f6e10c4371eddd65b005a698e5ad93928a2a0298
enes-kesen <52306218+enes-kesen@users.noreply.github.com>
2023-05-28 19:01:26 +0300

    Create CommunicationModule.cs

---
+++ CommunicationModule.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCC_TasarımO
+{
+    internal class CommunicationModule
+    {
+        public CommunicationModule()
+        {
+            // İletişim modülünü başlat
+        }
+
+        public void SendData(string data)
+        {
+            // Veriyi gönder
+            // Bu örnekte, bu bir yer tutucu ve gerçek bir veri gönderme işlemi içermiyor
+        }
+    }
+}
